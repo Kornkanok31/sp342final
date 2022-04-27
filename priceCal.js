618ec6430c1f8c59b99690a0b5237bb9387379fe
kornknaok <2010511101031@live4.utcc.ac.th>
2022-04-27 15:15:20 +0700

    priceCal

--- priceCal.js
+++ priceCal.js
@@ -2,8 +2,24 @@ function priceCalculate() {
   let ticknum = (document.getElementById("ticknum").value).trim();
   let selectObj = document.getElementById("event");
   let eventId = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  
-  return 0;
+  let price;
+ if(eventId == 1){
+	 price = 91;
+	 t = price*ticknum;
+ }
+ if(eventId == 2){
+	 price = 79;
+	 t = price*ticknum;
+ }
+  if(eventId == 3){
+	 price = 83;
+	 t = price*ticknum;
+ }
+  if(eventId == 4){
+	 price = 100;
+	 t = price*ticknum;
+ }
+  return t;
 }
 
 
